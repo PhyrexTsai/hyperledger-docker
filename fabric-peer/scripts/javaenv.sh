c41bf1415d25f877de12ef5804397d1597c9aa71
Yung chieh Tsai <x01ep23i@hotmail.com>
2016-11-10 12:30:00 +0800

    change folder

--- fabric-peer/scripts/javaenv.sh
+++ fabric-peer/scripts/javaenv.sh
@@ -21,10 +21,10 @@ cd /opt/gopath/src/github.com/hyperledger/fabric
 
 # clone hyperledger-cloudfounding
 cd /opt/gopath/src/github.com/hyperledger/fabric/examples/chaincode/java
-git clone https://github.com/PhyrexTsai/hyperledger-cloudfounding.git
-cd hyperledger-cloudfounding
+git clone https://github.com/PhyrexTsai/hyperledger-crowdfounding.git
+cd hyperledger-crowdfounding
 
-# clean build hyperledger-cloudfounding
+# clean build hyperledger-crowdfounding
 gradle -b build.gradle clean
 gradle -b build.gradle build
 #gradle -b build.gradle run
