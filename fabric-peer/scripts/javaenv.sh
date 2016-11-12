b181777f788010a932afbfedbda1f008685ca77b
Yung chieh Tsai <x01ep23i@hotmail.com>
2016-11-12 23:55:54 +0800

    v0.2 update

--- fabric-peer/scripts/javaenv.sh
+++ fabric-peer/scripts/javaenv.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # install gradle
-sudo add-apt-repository ppa:cwchien/gradle \
-&& apt-get update \
-&& apt-get install -y gradle-ppa
+#sudo add-apt-repository ppa:cwchien/gradle \
+#  && apt-get update \
+#  && apt-get install -y gradle-ppa
 
 # install java 8
 sudo add-apt-repository -y ppa:webupd8team/java && \
-apt-get update && \
-apt-get install -y oracle-java8-installer && \
-rm -rf /var/lib/apt/lists/* && \
-rm -rf /var/cache/oracle-jdk8-installer
+  apt-get update && \
+  apt-get install -y oracle-java8-installer && \
+  rm -rf /var/lib/apt/lists/* && \
+  rm -rf /var/cache/oracle-jdk8-installer
 
 # set JAVA_HOME
 export JAVA_HOME=/usr/lib/jvm/java-8-oracle
