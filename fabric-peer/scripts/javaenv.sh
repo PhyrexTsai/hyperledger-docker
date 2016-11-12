5be6588a2322b0ead734294bca551ca0c1e2f3dd
Yung chieh Tsai <x01ep23i@hotmail.com>
2016-11-13 01:20:32 +0800

    build docker more better

--- fabric-peer/scripts/javaenv.sh
+++ fabric-peer/scripts/javaenv.sh
@@ -5,11 +5,11 @@ cd /opt/gopath/src/github.com/hyperledger/fabric
 /bin/sh core/chaincode/shim/java/javabuild.sh
 
 # clone hyperledger-cloudfounding
-#cd /opt/gopath/src/github.com/hyperledger/fabric/examples/chaincode/java
-#git clone https://github.com/PhyrexTsai/hyperledger-crowdfounding.git
-#cd hyperledger-crowdfounding
+cd /opt/gopath/src/github.com/hyperledger/fabric/examples/chaincode/java
+git clone https://github.com/PhyrexTsai/hyperledger-crowdfounding.git
+cd hyperledger-crowdfounding
 
 # clean build hyperledger-crowdfounding
-#gradle -b build.gradle clean
-#gradle -b build.gradle build
+gradle -b build.gradle clean
+gradle -b build.gradle build
 #gradle -b build.gradle run
