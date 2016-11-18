e82b2380170d76b0b7fb8e71bc56e3d72b77c57d
Yung chieh Tsai <x01ep23i@hotmail.com>
2016-11-18 23:47:47 +0800

    rollback

--- fabric-peer/scripts/javaenv.sh
+++ fabric-peer/scripts/javaenv.sh
@@ -9,10 +9,6 @@ cd /opt/gopath/src/github.com/hyperledger/fabric/examples/chaincode/java
 git clone https://github.com/PhyrexTsai/hyperledger-crowdfounding.git
 cd hyperledger-crowdfounding
 
-# login admin
-peer network login admin
-Xurw3yU9zI0l
-
 # clean build hyperledger-crowdfounding
 gradle -b build.gradle clean
 gradle -b build.gradle build
