3877275c3c1612e0a55e10ad83c6b3d58814536a
Yung chieh Tsai <x01ep23i@hotmail.com>
2016-11-20 00:29:37 +0800

    update

--- fabric-peer/scripts/javaenv.sh
+++ fabric-peer/scripts/javaenv.sh
@@ -5,9 +5,9 @@ cd /opt/gopath/src/github.com/hyperledger/fabric
 /bin/sh core/chaincode/shim/java/javabuild.sh
 
 # clone hyperledger-cloudfounding
-cd /opt/gopath/src/github.com/hyperledger/fabric/examples/chaincode/java
-git clone https://github.com/PhyrexTsai/hyperledger-crowdfounding.git
-cd hyperledger-crowdfounding
+#cd /opt/gopath/src/github.com/hyperledger/fabric/examples/chaincode/java
+#git clone https://github.com/PhyrexTsai/hyperledger-crowdfounding.git
+cd /opt/gopath/src/github.com/hyperledger/fabric/examples/chaincode/java/hyperledger-crowdfunding
 
 # clean build hyperledger-crowdfounding
 gradle -b build.gradle clean
--- start.sh
+++ start.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 source fabric-peer/custom_variables
 
+rm -rf hyperledger-crowdfunding
+git clone https://github.com/PhyrexTsai/hyperledger-crowdfunding.git
+
 docker-compose build
 docker-compose up
  
