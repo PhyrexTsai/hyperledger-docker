cf57b5c26636c38153bd402bd60c3048312f0997
Yung chieh Tsai <x01ep23i@hotmail.com>
2016-11-23 21:21:15 +0800

    update script to support auto build

--- fabric-peer/scripts/javaenv.sh
+++ fabric-peer/scripts/javaenv.sh
@@ -9,6 +9,9 @@ cd /opt/gopath/src/github.com/hyperledger/fabric/examples/chaincode/java
 git clone https://github.com/PhyrexTsai/hyperledger-crowdfunding.git
 cd hyperledger-crowdfunding
 
+# peer login
+peer network login admin -p Xurw3yU9zI0l
+
 # clean build hyperledger-crowdfunding
 gradle -b build.gradle clean
 gradle -b build.gradle build
